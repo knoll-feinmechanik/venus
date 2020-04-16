242ed74c718c7909bc2316fb3f5dd6ce79714517
Wiebe Cazemier <wiebe@ytec.nl>
2020-04-16 12:37:49 +0200

    Do API call to CD Networks to prefetch feed

---
+++ cdn-prefetch.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+#
+# Script to pre-fetch Venus images into CDN cache.
+#
+# Wiebe Cazemier <wiebe@ytec.nl>. Approved by 'shellcheck'.
+
+password_filepath="$HOME/.cdnetworks-victronci-api-key"
+docroot="/var/www/victron_www"
+sshuser="victron_www"
+originserver="updates-origin.victronenergy.com"
+
+if [[ ! -f "$password_filepath" ]]; then
+  echo "Put the API key in $password_filepath. Ask Ytec (wiebe) to give it to you."
+  exit 1
+fi
+
+chmod 600 "$password_filepath"
+
+if ! which curl &> /dev/null; then
+  echo "install curl"
+  exit 1
+fi
+
+if ! which openssl &> /dev/null; then
+  echo "install openssl"
+  exit 1
+fi
+
+if [[ "$1" == "--check-mode" ]]; then
+  exit 0
+fi
+
+set -u
+
+docroot=$(echo "$docroot" | sed -e 's#/$##')
+urls=$(ssh "$sshuser@$originserver" "find '$docroot/feeds/venus/' -type l -print0 | xargs -0 -I '{}' echo '{}' | sed -e 's#$docroot#https://updates.victronenergy.com#' | sed -e 's/ /%20/g'")
+
+OIFS="$IFS"
+IFS=$'\n'
+comma=""
+all_urls=""
+for url in $urls; do
+  all_urls="$all_urls ${comma} \"$url\" "
+  comma=","
+done
+IFS="$OIFS"
+
+usename='victronci'
+apikey=$(cat "$password_filepath")
+date=$(LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8" date -u "+%a, %d %b %Y %H:%M:%S GMT")
+password=$(echo -en "$date" | openssl dgst -sha1 -hmac "$apikey" -binary | openssl enc -base64)
+
+curl_output=$(curl --silent --include --url "https://api.cdnetworks.com/ccm/fetch/ItemIdReceiver" \
+  --user "$usename:$password" \
+  --header "Date: $date" \
+  --header 'Accept: application/json' \
+  --header 'Content-Type: application/json' \
+  --header "Expect:" \
+  --user-agent "$0" \
+  --data "{ \"urls\" : [ ${all_urls} ] }"
+)
+
+curl_result=$?
+
+if [[ "$curl_result" -ne 0 ]]; then
+  echo "Curl itself failed. Probably connection error?"
+  echo
+  echo "$curl_output"
+  exit 1
+fi
+
+first_line=$(echo "$curl_output" | head -n 1)
+
+if [[ ! "$first_line" == *200* ]]; then
+  echo "Not HTTP 200. Failing"
+  echo
+  echo "$curl_output"
+  echo
+  echo "Again, not HTTP 200. Failing"
+  exit 1
+fi
+
+if ! echo "$curl_output" | grep --quiet --extended-regexp --ignore-case '"code".{0,4}1'; then
+  echo "Code 1 not found, must be an error then"
+  echo
+  echo "$curl_output"
+  echo
+  echo "Again, code 1 not found, must be an error then"
+  exit 1
+fi
+
+echo
+echo "$curl_output"
+echo
+echo "CDN pre-fetching API call seemingly successful"
+exit 0
--- do_release.sh
+++ do_release.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
 DEPLOY=deploy/venus
 REMOTE=victron_www@updates-origin.victronenergy.com
 D=/var/www/victron_www/feeds/venus/
 FEED="$REMOTE:$OPKG"
+
+CURRENT_FILEPATH=$(readlink --canonicalize "$0")
+CURRENT_DIR=$(dirname "$CURRENT_FILEPATH")
+PREFETCH_SCRIPT="$CURRENT_DIR/cdn-prefetch.sh"
 
 if [ $# -eq 0 ]; then
 	echo "Usage: $0 release|candidate|testing"
@@ -13,6 +19,19 @@ function release ()
 	from="$D$1"
 	to="$D$2"
 	exclude=""
+
+	prefetch_cache="false"
+
+	if [[ "$2" == "release" || "$2" == "candidate" ]]; then
+		prefetch_cache="true"
+	fi
+
+	if "$prefetch_cache" ; then
+		if ! "$PREFETCH_SCRIPT" --check-mode; then
+			echo "CDN prefetch conditions not met"
+			exit 1
+		fi
+	fi
 
 	echo $from $to
 	ssh $REMOTE "if [ ! -d $to ]; then mkdir $to; fi"
@@ -29,6 +48,10 @@ function release ()
 	fi
 
 	ssh $REMOTE "rsync -v $exclude -rpt --delete $from/ $to"
+
+	if "$prefetch_cache" ; then
+		$PREFETCH_SCRIPT
+	fi
 }
 
 case $1 in
@@ -58,3 +81,5 @@ case $1 in
 		echo "Not a valid parameter"
 		;;
 esac
+
+# vim: noexpandtab:shiftwidth=2:tabstop=2:softtabstop=0:textwidth=110
